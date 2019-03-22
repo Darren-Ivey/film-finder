85ae40f7c4c0927de91a7b0fdf6003e6d5c03139
Darren Ivey <darren.ivey@aerian.com>
2019-03-22 17:19:47 +0000

    Use hooks on search page

--- src/page/searchPage.js
+++ src/page/searchPage.js
@@ -1,36 +1,72 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import Form from '../components/form';
 import Results from '../components/results';
 import Error from '../components/error';
 import MyFilms from '../components/myfilms';
 
 const SearchPage = () => {
-    const [searchTerm, setSearchTerm] = useState(undefined);
-    const [results, setResults] = useState(undefined);
-    const [searchError, setSearchError] = useState(false);
-    const [serviceError, setServiceError] = useState(false);
-    const [searchErrorMsg, setSearchErrorMsg] = useState("");
-    const [selectedFilms, setSelectedFilms] = useState([]);
+    const initialState = {
+        searchTerm: undefined,
+        results: undefined,
+        searchError: false,
+        serviceError: false,
+        searchErrorMsg: "",
+        selectedFilms: [],
+    };
+
+    const stateReducer = (state, {type, payload}) => {
+        switch (type) {
+        case 'SEARCH_SUCCESS':
+            return {
+                ...state, 
+                results: payload,
+                searchError: false,
+                serviceError: false,
+                searchErrorMsg: "",
+            };
+        case 'SEARCH_FAIL':
+            return {
+                ...state,
+                results: null,
+                serviceError: true,
+                searchErrorMsg: payload,
+            };
+        case 'SELECT_FILM':
+            return {
+            ...state,
+            selectedFilms: [...state.selectedFilms, payload],
+        };
+        case 'SET_SEARCH_TERM':
+            return {
+            ...state,
+            searchTerm: payload,
+        };
+          default:
+            throw new Error();
+        }
+    }
+
+    const [state, dispatch] = useReducer(stateReducer, initialState);
 
     const handleResponse = (response) => {
         if (!response.Error) {
-            setSearchError(false);
-            setServiceError(false);
-            setSearchErrorMsg("");
-            setResults(response.Search);
+            dispatch({type: 'SEARCH_SUCCESS', payload: response.Search});
         } else {
-            setSearchError(true);
-            setSearchErrorMsg(response.Error);
+            dispatch({type: 'SEARCH_FAIL', payload: response.Error});
         }
     }
 
     const addToMyList = (film) => {    
         const isAlreadyInList = (film) =>
-            selectedFilms.some((selectedFilm) =>
+            state.selectedFilms.some((selectedFilm) =>
                 film.imdbID === selectedFilm.imdbID);
         if (!isAlreadyInList(film)) {
-            setSelectedFilms([...selectedFilms, film]);
+            dispatch({type: 'SELECT_FILM', payload: film});
         }
+    }
+
+    const setSearchTerm = (searchTerm) => {
+        dispatch({type: 'SET_SEARCH_TERM', payload: searchTerm});
     }
 
     return (
@@ -38,17 +74,17 @@ const SearchPage = () => {
             <Form
                 setParentSearchTerm={setSearchTerm}
                 handleResponse={handleResponse} />
-                { serviceError && <Error /> }
+                { state.serviceError && <Error /> }
             <div className="film-view">
                 <Results
                     handleResponse={handleResponse}
-                    searchError={searchError}
-                    searchErrorMsg={searchErrorMsg}
-                    results={results}
+                    searchError={state.searchError}
+                    searchErrorMsg={state.searchErrorMsg}
+                    results={state.results}
                     addToMyList={addToMyList}
-                    searchTerm={searchTerm}/>
+                    searchTerm={state.searchTerm}/>
                 <MyFilms
-                    films={selectedFilms}/>
+                    films={state.selectedFilms}/>
             </div>
         </div>
     )
